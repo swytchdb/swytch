476d6a11c8add50fc55540e41943e0e8e0471911
Robert Landers <landers.robert@gmail.com>
2026-05-04 18:49:21 +0200

    better estimate
    Signed-off-by: Robert Landers <landers.robert@gmail.com>

--- effects/effect.go
+++ effects/effect.go
@@ -234,7 +234,7 @@ func NewEngine(cfg EngineConfig) *Engine {
 		voidedBinds: xsync.NewMap[string, struct{}](),
 	}
 	e.effectCache.SetSizeFunc(func(_ keytrie.EffectRef, v *pb.Effect) int64 {
-		return int64(proto.Size(v))
+		return 16 + int64(proto.Size(v)) // 16 = sizeof(EffectRef=[2]uint64)
 	})
 	e.cache = cache
 
